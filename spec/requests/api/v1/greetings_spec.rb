14606d99f389e669ac531d95325a1bd9e73141a3
nedjwab <62156607+nedjwab@users.noreply.github.com>
2022-10-06 13:00:21 +0100

    Fix rubocop errors

--- spec/requests/api/v1/greetings_spec.rb
+++ spec/requests/api/v1/greetings_spec.rb
@@ -1,11 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe "Api::V1::Greetings", type: :request do
-  describe "GET /index" do
-    it "returns http success" do
-      get "/api/v1/greetings/index"
+RSpec.describe 'Api::V1::Greetings', type: :request do
+  describe 'GET /index' do
+    it 'returns http success' do
+      get '/api/v1/greetings/index'
       expect(response).to have_http_status(:success)
     end
   end
-
 end
