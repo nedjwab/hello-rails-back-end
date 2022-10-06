5640d8d7b078c5252144bd120e875b1c299fea76
nedjwab <62156607+nedjwab@users.noreply.github.com>
2022-10-06 12:39:08 +0100

    Create greeting model

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      ressources :greetings
+      resources :greetings
     end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
