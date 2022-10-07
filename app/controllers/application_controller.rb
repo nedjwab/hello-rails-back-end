d01e003154e3dc3951f7a948d0e631f8c8bdf319
nedjwab <62156607+nedjwab@users.noreply.github.com>
2022-10-07 14:26:47 +0100

    Fix corse issues

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::API
+    before_action :set_headers_and_options
+
+  def set_headers_and_options
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  end
 end
